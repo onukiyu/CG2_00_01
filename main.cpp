9c187dbd7874a57f49f5e871154efa809cd34104
onukiyu <k023g0017@g.neec.ac.jp>
2024-04-16 10:57:43 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <Windows.h>
+
+//Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+	//出力ウインドウへの文字出力
+	OutputDebugStringA("Hello,DirectX!\n");
+
+	return 0;
+}
